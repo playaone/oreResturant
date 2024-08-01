add6f70a4859d5f6fe808ba4dc79de6ab0874f0f
Nwankwo Christopher <dev.christopher007@gmail.com>
2024-08-02 00:28:54 +0100

    changing port and host

--- run.py
+++ run.py
@@ -2,5 +2,8 @@ from app import create_app
 
 app = create_app()
 
+# 54.191.253.12
+# 44.226.122.3
+
 if __name__ == '__main__':
-    app.run(debug=True, host='54.191.253.12')
+    app.run(debug=True, host='0.0.0.0', port=5000)
